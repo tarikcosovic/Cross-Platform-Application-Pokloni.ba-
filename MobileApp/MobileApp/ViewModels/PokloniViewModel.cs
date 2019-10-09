e1512720648a1c2cfaaebb5d0da0a31f06dc776e
Tarik Ćosović <tarikcosovic@gmail.com>
2019-10-09 16:10:57 +0200

    Latest update before deploying db and api

---
+++ MobileApp/MobileApp/ViewModels/PokloniViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MobileApp.ViewModels
+{
+    public class PokloniViewModel
+    {
+        public void CreateTable()
+        {
+            TableView list = new TableView();
+            list.RowHeight = 80;
+            TableRoot troot = new TableRoot();
+            list.Root = troot;
+            TableSection section = new TableSection();
+            troot.Add(section);
+        }
+    }
+}
---
+++ MobileApp/MobileApp/Views/PokloniPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MobileApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PokloniPage : ContentPage
+    {
+        private PokloniViewModel model = null;
+        public PokloniPage()
+        {
+            InitializeComponent();
+            BindingContext = model = new PokloniViewModel();
+        }
+
+        private void ImageCell_Tapped(int id)
+        {
+
+        }
+    }
+}
